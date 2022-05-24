4269681d26d3a0cb5d4cd4a2644362642513cbac
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-05-25 00:46:46 +0530

    eighth commit

---
+++ src/packages/MainClass.java
@@ -0,0 +1,14 @@
+package packages;
+
+import packages.mymathpackage.Maths;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("PI = " + Maths.PI);
+
+        int a = 10, b = 20;
+        System.out.println("Minimum = " + Maths.getMin(a, b));
+        System.out.println("Maximum = " + Maths.getMax(a, b));
+    }
+}
---
+++ src/packages/mymathpackage/Maths.java
@@ -0,0 +1,16 @@
+package packages.mymathpackage;
+
+public class Maths {
+
+    public static final double PI = 3.141592653589793;
+
+    public static int getMin(int a, int b){
+        if(a < b) return a;
+        else return b;
+    }
+
+    public static int getMax(int a, int b){
+        if(a > b) return a;
+        else return b;
+    }
+}
