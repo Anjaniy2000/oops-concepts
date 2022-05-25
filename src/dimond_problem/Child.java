0b031305b72dbfc86e452f036b8150ec46b66abe
Anjaniy Salekar <anjaniy01salekar@gmail.com>
2022-05-25 14:39:31 +0530

    Update Child.java

--- src/dimond_problem/Child.java
+++ src/dimond_problem/Child.java
@@ -1,5 +1,6 @@
 package dimond_problem;
 
+//This Will Give Compile Time Error(Can't Inherit More Than One Class).
 public class Child extends Parent1, Parent2{
     public void walk(){
         System.out.println("Walking");
